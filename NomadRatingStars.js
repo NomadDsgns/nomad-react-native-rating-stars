8edd3d40890a175e8074efd78daad36370759de5
Daniel Hawkins <90428134+NomadDsgns@users.noreply.github.com>
2021-09-11 22:40:29 -0500

    Create NomadRatingStars.js

---
+++ NomadRatingStars.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+
+export default function({ stars, size, style }) {
+    const list = [];
+    const sortStars = () => {
+        let count = 0;
+        while (count < stars) {
+            list.push("star");
+            count++;
+        }
+        while (count < 5) {
+            list.push("star-o")
+            count++;
+        }
+    }
+    sortStars();
+    
+
+    return (
+        <View style={{ flexDirection: "row", justifyContent: "center", ...style }}>
+            {
+                list.map((star, index) => {
+                    return (<FontAwesome style={styles.star} name={star} key={index} size={size ? size : 24} color="black" />)
+                })
+            }
+        </View>
+    );
+}
+
+export default RatingSarsView;
+
+const styles = StyleSheet.create({
+      textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+      },
+      star: {
+        paddingLeft: 2,
+        paddingRight: 2
+      },
+});
